1016ce697fd281af9f59199d91860f5b35458b96
uirleandr0 <uirleandro.santos.19@gmail.com>
2023-02-12 20:00:36 -0300

    usando js pela primeira vez

---
+++ calculadora.js
@@ -0,0 +1,20 @@
+function inserir(num){
+    let valor = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = valor + num;
+}
+
+function resetar(){
+    document.getElementById('resultado').innerHTML = '';
+}
+
+function apagar(){
+    let resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
+}
+
+function somar(){
+    let resultado = document.getElementById('resultado').innerHTML;
+    if(resultado){
+        document.getElementById('resultado').innerHTML = eval(resultado);
+    }
+}
